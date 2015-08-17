ca67de00dc47b64299d6e68cddf90486d6e5eac2
Brandon Black <brandonmblack@gmail.com>
2015-08-16 22:12:55 -0700

    [bug] resolves time rounding issues in #8

--- lib/twitter-ads/utils.rb
+++ lib/twitter-ads/utils.rb
@@ -5,10 +5,25 @@ module TwitterAds
 
     class << self
 
+      # Helper to convert objects into boolean values.
+      #
+      # @param object [Object] The object to be converted.
+      #
+      # @return [Boolean] The boolean result.
+      #
+      # @since 0.1.0
       def to_bool(object)
         (object.to_s.downcase == 'false') ? false : !!object
       end
 
+      # Helper to convert a time object according to a given granularity level.
+      #
+      # @param time [Time] A valid Time instance.
+      # @param granularity [Symbol] A symbol representing the desired granuarlity (eg. :hour).
+      #
+      # @return [Time] The formatted Time instance.
+      #
+      # @since 0.1.0
       def to_time(time, granularity = nil)
         return time.iso8601 unless granularity
         if granularity == :hour
@@ -20,6 +35,13 @@ module TwitterAds
         end
       end
 
+      # Converts key names to symbols on a given object.
+      #
+      # @param object [Object] The object to be converted.
+      #
+      # @return [Object] The symbolized, converted object.
+      #
+      # @since 0.1.0
       def symbolize!(object)
         if object.is_a?(Array)
           object.each_with_index { |value, index| object[index] = symbolize!(value) }
---
+++ spec/twitter-ads/utils_spec.rb
@@ -0,0 +1,100 @@
+# Copyright (C) 2015 Twitter, Inc.
+
+require 'spec_helper'
+
+describe TwitterAds::Utils do
+
+  describe '#to_bool' do
+
+    it 'successfully coerces "nil" to its correct boolean value' do
+      expect(subject.to_bool(nil)).to be false
+    end
+
+    it 'successfully coerces "true" to its correct boolean value' do
+      expect(subject.to_bool(true)).to be true
+    end
+
+    it 'successfully coerces "false" to its correct boolean value' do
+      expect(subject.to_bool(false)).to be false
+    end
+
+    it 'does not coerces numerical values' do
+      expect(subject.to_bool(0)).to be true
+      expect(subject.to_bool(1)).to be true
+      expect(subject.to_bool(999)).to be true
+    end
+
+  end
+
+  describe '#to_time' do
+
+    let(:time) { Time.now }
+
+    context 'when a granularity is not specified' do
+
+      it 'returns the ISO 8601 formatted string' do
+        expect(subject.to_time(time)).to eq(time.iso8601)
+      end
+
+    end
+
+    context 'when a granularity is specified' do
+
+      it 'returns the ISO 8601 formatted string for :hour rounded to the hour' do
+        expected_result = Time.new(time.year, time.month, time.day, time.hour).iso8601
+        expect(subject.to_time(time, :hour)).to eq(expected_result)
+      end
+
+      it 'returns the ISO 8601 formatted string for :day rounded to the day' do
+        expected_result = Time.new(time.year, time.month, time.day).iso8601
+        expect(subject.to_time(time, :day)).to eq(expected_result)
+      end
+
+      it 'returns the ISO 8601 formatted string for all other granularities' do
+        expect(subject.to_time(time, :total)).to eq(time.iso8601)
+      end
+
+    end
+
+  end
+
+  describe '#symbolize!' do
+
+    context 'with a simple object' do
+
+      let(:simple) { { 'key1' => 'foo', 'key2' => 'bar', 'key3' => 1234 } }
+
+      it 'successfully converts all keys to symbols' do
+        result = subject.symbolize!(simple)
+        expect(result.keys).to all(be_a(Symbol))
+      end
+
+    end
+
+    context 'with a nested object' do
+
+      let(:nested) { { 'key1' => 'foo', 'key2' => { 'key3' => 1 } } }
+
+      it 'successfully converts the nested object keys to symbols' do
+        result = subject.symbolize!(nested)
+        expect(result.keys).to all(be_a(Symbol))
+        expect(result[:key2].keys).to all(be_a(Symbol))
+      end
+
+    end
+
+    context 'with a nested collection of objects' do
+
+      let(:nested) { { 'key1' => 'foo', 'key2' => [ { 'key3' => 1 } ] } }
+
+      it 'successfully converts the collection of objects keys to symbols' do
+        result = subject.symbolize!(nested)
+        expect(result.keys).to all(be_a(Symbol))
+        expect(result[:key2][0].keys).to all(be_a(Symbol))
+      end
+
+    end
+
+  end
+
+end
