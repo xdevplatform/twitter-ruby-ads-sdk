d39248bdd25ce84baa99bee5911b462963185ff6
Brandon Black <brandonmblack@gmail.com>
2015-08-17 20:46:51 -0700

    [minor] adding docs for spec helper methods

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -3,7 +3,7 @@
 # Helper module for custom test matchers and utilities.
 module Helpers
 
-  # A utility method to temporarily redirect stdout and stderr for
+  # Helper method to temporarily redirect stdout and stderr for
   # a given block of code.
   #
   # @param &block [Block] The code block to be executed.
@@ -19,7 +19,7 @@ module Helpers
     $stderr = original_stderr
   end
 
-  # Utility method for returning a JSON fixture file.
+  # Helper method for returning a JSON fixture file.
   #
   # @param name [Symbol] The name of the fixture file to be returned.
   #
@@ -28,8 +28,14 @@ module Helpers
     File.new(File.expand_path("../../fixtures/#{name}.json", __FILE__))
   end
 
-  def stub_fixture(method, name, url)
-    stub_request(method, url).to_return(body: fixture(name), status: 200)
+  # Helper method to stub a request with a fixture by name.
+  #
+  # @param method [Symbol] The HTTP method (:get, :put, :post, :delete)
+  # @param name [String] The fixture name.
+  # @param url [String] The URL match pattern or regular expression.
+  # @param status [Integer] The response code to return with the fixture.
+  def stub_fixture(method, name, url, status = 200)
+    stub_request(method, url).to_return(body: fixture(name), status: status)
   end
 
 end
