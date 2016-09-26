b525e4ceec3ee65e4d6f4db13199f5e16eb5465c
kambayashia <kambayashia@gmail.com>
2016-09-26 16:14:03 +0900

    append bid_amount_local_micro if update campaign

--- lib/twitter-ads/campaign/line_item.rb
+++ lib/twitter-ads/campaign/line_item.rb
@@ -92,8 +92,8 @@ module TwitterAds
       params.delete(:bid_type) if params.has_key?(:automatically_select_bid)
 
       # If set to true, bid_amount_local_micro must be NULL
-      params.store(:bid_amount_local_micro, nil) if params[:automatically_select_bid]
-
+      params.store(:bid_amount_local_micro, nil) if params[:automatically_select_bid] && self.id.present?
+      
       # advertiser_user_id is currently beta-only and causes an error when sent.
       params.delete(:advertiser_user_id)
 
