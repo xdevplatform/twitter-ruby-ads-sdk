e42a1d6f7fe46b0f5d598e20a74b42244bdd6f47
Bob Cao <bobintornado@gmail.com>
2016-11-21 16:56:16 +0800

    Add Organic Tweet for async job (#131)

--- lib/twitter-ads.rb
+++ lib/twitter-ads.rb
@@ -37,6 +37,7 @@ require 'twitter-ads/campaign/targeting_criteria'
 require 'twitter-ads/campaign/tweet'
 require 'twitter-ads/campaign/organic_tweet'
 require 'twitter-ads/campaign/iab_category'
+require 'twitter-ads/campaign/organic_tweet'
 
 require 'twitter-ads/enum'
 
