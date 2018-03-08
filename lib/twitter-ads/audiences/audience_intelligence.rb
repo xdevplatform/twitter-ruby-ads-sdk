d493789fe107acb85d3025008daf958805eb04b0
Tushar Bhushan <tbhushan@twitter.com>
2018-03-08 15:04:55 -0800

    fixed code style issues

--- lib/twitter-ads/audiences/audience_intelligence.rb
+++ lib/twitter-ads/audiences/audience_intelligence.rb
@@ -23,42 +23,44 @@ module TwitterAds
     property :targeting_type, read_only: true
     property :targeting_value, read_only: true
     property :localized, read_only: true
-    
-    RESOURCE_CONVERSATIONS  = "/#{TwitterAds::API_VERSION}/" +
-                           'accounts/%{account_id}/audience_intelligence/conversations'.freeze # @api private
-    RESOURCE_DEMOGRAPHICS   = "/#{TwitterAds::API_VERSION}/" +
-                           'accounts/%{account_id}/audience_intelligence/demographics'.freeze # @api private
-    
+
+    RESOURCE_CONVERSATIONS  = "/#{TwitterAds::API_VERSION}/" \
+                              'accounts/%{account_id}/audience_intelligence/' \
+                              'conversations'.freeze # @api private
+    RESOURCE_DEMOGRAPHICS   = "/#{TwitterAds::API_VERSION}/" \
+                              'accounts/%{account_id}/audience_intelligence/' \
+                              'demographics'.freeze # @api private
 
     def initialize(account)
       @account = account
       self
     end
 
-    
-    def conversations()
+    def conversations
       headers = { 'Content-Type' => 'application/json' }
       params = {
-        conversation_type: self.conversation_type,
-        audience_definition: self.audience_definition,
-        targeting_inputs: self.targeting_inputs
+        conversation_type: conversation_type,
+        audience_definition: audience_definition,
+        targeting_inputs: targeting_inputs
       }
-      resource = RESOURCE_CONVERSATIONS % { account_id: self.account.id }
-      request = Request.new(self.account.client, :post, resource, body: params.to_json, headers: headers)
-      Cursor.new(self.class, request, init_with: [self.account])
+      resource = RESOURCE_CONVERSATIONS % { account_id: account.id }
+      request = Request.new(account.client, :post, resource, body: params.to_json, headers: headers)
+      Cursor.new(self.class, request, init_with: [account])
     end
 
-    def demographics()
+    def demographics
       headers = { 'Content-Type' => 'application/json' }
       params = {
-        audience_definition: self.audience_definition,
-        targeting_inputs: self.targeting_inputs
+        audience_definition: audience_definition,
+        targeting_inputs: targeting_inputs
       }
-      resource = RESOURCE_DEMOGRAPHICS % { account_id: self.account.id }
-      response = Request.new(self.account.client, :post, resource, body: params.to_json, headers: headers).perform
-      response = response.body[:data]
+      resource = RESOURCE_DEMOGRAPHICS % { account_id: account.id }
+      response = Request.new(account.client, :post,
+         resource,
+         body: params.to_json,
+         headers: headers).perform
+      response.body[:data]
       # cannot use cursor here given that the response "keys" are dynmaic based on input values
     end
   end
 end
-     
