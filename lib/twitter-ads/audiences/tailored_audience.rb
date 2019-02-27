d2fc3afdf529dc1b88029966d2150d407232e586
snobell <chris.scobell@gmail.com>
2019-02-27 15:17:23 +1100

    Make Rubocop happy

--- lib/twitter-ads/audiences/tailored_audience.rb
+++ lib/twitter-ads/audiences/tailored_audience.rb
@@ -34,8 +34,8 @@ module TwitterAds
     RESOURCE_MEMBERSHIPS = "/#{TwitterAds::API_VERSION}/" +
                            'tailored_audience_memberships'.freeze # @api private
     RESOURCE_USERS       = "/#{TwitterAds::API_VERSION}/" +
-                           "accounts/%{account_id}/tailored_audiences/" +
-                           "%{id}/users".freeze # @api private
+                           'accounts/%{account_id}/tailored_audiences/' +
+                           '%{id}/users'.freeze # @api private
     # @api private
     GLOBAL_OPT_OUT = "/#{TwitterAds::API_VERSION}/" +
                      'accounts/%{account_id}/tailored_audiences/global_opt_out'.freeze
