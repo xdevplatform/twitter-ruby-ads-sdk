75cea602036d68b1b06eb283f30295ced105302c
Tushar Bhushan <tbhushan@twitter.com>
2019-03-04 13:35:45 -0800

    added support for card_uris

--- lib/twitter-ads/creative/cards_fetch.rb
+++ lib/twitter-ads/creative/cards_fetch.rb
@@ -73,7 +73,7 @@ module TwitterAds
       property :id, read_only: true
 
       FETCH_URI = "/#{TwitterAds::API_VERSION}/" +
-                  'accounts/%{account_id}/cards'.freeze # @api private
+                  'accounts/%{account_id}/cards/all'.freeze # @api private
       FETCH_ID  = "/#{TwitterAds::API_VERSION}/" +
                   'accounts/%{account_id}/cards/all/%{id}'.freeze # @api private
 
@@ -82,26 +82,27 @@ module TwitterAds
           "'CardsFetch' object has no attribute 'all'")
       end
 
-      def load(account, card_uri = nil, card_id = nil, with_deleted = nil, opts = {})
-        if (card_uri && card_id) || (card_uri.nil? && card_id.nil?)
+      def load(account, card_uris = nil, card_id = nil, with_deleted = nil, opts = {})
+        if (card_uris && card_id) || (card_uris.nil? && card_id.nil?)
           raise ArgumentError.new('card_uri and card_id are exclusive parameters. ' \
                              'Please supply one or the other, but not both.')
-        end
-
-        if card_uri
-          params = { card_uri: card_uri }.merge!(opts)
-          resource = FETCH_URI % { account_id: account.id }
-        else
-          params = { card_id: card_id }.merge!(opts)
-          resource = FETCH_ID % { account_id: account.id, id: card_id }
         end
 
         if with_deleted && TwitterAds::Utils.to_bool(with_deleted)
           params = { with_deleted: true }.merge!(opts)
         end
 
-        response = Request.new(account.client, :get, resource, params: params).perform
-        from_response(response.body[:data])
+        if card_uris
+          params = { card_uris: card_uris }.merge!(opts)
+          resource = FETCH_URI % { account_id: account.id }
+          request = Request.new(account.client, :get, resource, params: params)
+          return Cursor.new(self.class, request, init_with: [account])
+        else
+          params = { card_id: card_id }.merge!(opts)
+          resource = FETCH_ID % { account_id: account.id, id: card_id }
+          response = Request.new(account.client, :get, resource, params: params).perform
+          return from_response(response.body[:data])
+        end
       end
 
       def reload!
