99c4719f9dd25129d87019cdc36025befd0e3fd5
Tushar Bhushan <tbhushan@twitter.com>
2020-03-16 12:47:58 -0700

    added advertiser business categories endpoint

---
+++ lib/twitter-ads/campaign/advertiser_business_categories.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+# Copyright (C) 2019 Twitter, Inc.
+
+module TwitterAds
+  class AdvertiserBusinessCategories
+
+    include TwitterAds::DSL
+    include TwitterAds::Resource
+
+    attr_reader :account
+
+    property :id, read_only: true
+    property :name, read_only: true
+    property :iab_categories, read_only: true
+
+    RESOURCE_COLLECTION = "/#{TwitterAds::API_VERSION}/advertiser_business_categories" # @api private
+
+    def initialize(account)
+      @account = account
+      self
+    end
+  end
+end
