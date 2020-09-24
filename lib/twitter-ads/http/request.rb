b57bb012b6e380aef71ebb1e441042473872f375
Tom Osowski <tosowski@twitter.com>
2020-09-24 16:06:03 -0700

    fix up code quality

--- lib/twitter-ads/http/request.rb
+++ lib/twitter-ads/http/request.rb
@@ -110,7 +110,7 @@ module TwitterAds
     def escape_params(input)
       input.map do |key, value|
         "#{CGI.escape key.to_s}=#{CGI.escape value.to_s}"
-      end.join("&")
+      end.join('&')
     end
 
     def http_request
